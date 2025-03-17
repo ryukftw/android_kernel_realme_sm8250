623c3e9e30cd90fac5ba64239ff10b88e56974e4
ryukftw <ryukv009@gmail.com>
2025-03-17 02:25:48 +0000

    Let's call it phoeniX-next

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ cd anykernel || exit 1
     cp ../out/arch/arm64/boot/Image .
     cp ../out/arch/arm64/boot/dtbo.img .
     cp ../out/arch/arm64/boot/dtb .
-    zip -r9 phoeniX-AOSP-${DT}.zip *
+    zip -r9 phoeniX-next-${DT}.zip *
     rm Image dtbo.img dtb
     cd ..
 }
