ccfa8ecd12dd4f07d9f8b4b58e1f03a44087e6e3
ryukftw <ryukv009@gmail.com>
2025-04-10 08:25:16 +0000

    Revert "usb: dwc3: Increase DWC3 controller halt timeout"
    This reverts commit 5a3f334a76a043dcdbc4f354e12b2c94d1f99687.

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2284,7 +2284,7 @@ done:
 static int dwc3_gadget_run_stop(struct dwc3 *dwc, int is_on, int suspend)
 {
 	u32			reg, reg1;
-	u32			timeout = 2000;
+	u32			timeout = 1500;
 #ifdef OPLUS_FEATURE_CHG_BASIC
 	ktime_t		start, diff;
 #endif
@@ -2377,7 +2377,6 @@ static int dwc3_gadget_run_stop(struct dwc3 *dwc, int is_on, int suspend)
 	}
 
 	do {
-		usleep_range(1000, 2000);
 		reg = dwc3_readl(dwc->regs, DWC3_DSTS);
 		reg &= DWC3_DSTS_DEVCTRLHLT;
 	} while (--timeout && !(!is_on ^ !reg));
