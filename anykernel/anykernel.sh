ryukftw/kernel_realme_rmx3371

anykernel: Bake AnyKernel3 into the kernel tree

anykernel/anykernel.sh
@@ -1,0 +1,38 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=phoeniX Kernel by Ryuk
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=RMX3371
+device.name2=RE54E4L1
+device.name3=spartan
+device.name4=
+device.name5=
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel boot install
+dump_boot;
+
+vbmeta_disable_verification;
+
+write_boot;
+## end boot install

ryukftw/android_kernel_realme_sm8250

Change-Id: I6cc0b9d699b4e59b1db2daeeed8cc50349c161b5

anykernel/anykernel.sh
@@ -1,0 +1,38 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=phoeniX Kernel by Ryuk
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=RMX3370
+device.name2=RMX3371
+device.name3=
+device.name4=
+device.name5=
+supported.versions=12-16
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel boot install
+dump_boot;
+
+vbmeta_disable_verification;
+
+write_boot;
+## end boot install