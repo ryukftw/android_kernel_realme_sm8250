160fe6ecee586d65266ee9db6617e59a874e6a1b
bijoyv9 <bijoyv9@gmail.com>
2026-05-23 07:58:59 +0000

    KSU: Add manual hooks

--- fs/exec.c
+++ fs/exec.c
@@ -1903,6 +1903,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+			       void *argv, void *envp, int *flags);
+#endif
+
 int do_execve_file(struct file *file, void *__argv, void *__envp)
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
@@ -1915,9 +1921,14 @@ int do_execve(struct filename *filename,
 	const char __user *const __user *__argv,
 	const char __user *const __user *__envp)
 {
+	int fd = AT_FDCWD;
+	int flags = 0;
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
-	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
+	return do_execveat_common(fd, filename, argv, envp, flags);
 }
 
 int do_execveat(int fd, struct filename *filename,
@@ -1936,6 +1947,8 @@ static int compat_do_execve(struct filename *filename,
 	const compat_uptr_t __user *__argv,
 	const compat_uptr_t __user *__envp)
 {
+	int fd = AT_FDCWD;
+	int flags = 0;
 	struct user_arg_ptr argv = {
 		.is_compat = true,
 		.ptr.compat = __argv,
@@ -1944,7 +1957,10 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
-	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+#endif
+	return do_execveat_common(fd, filename, argv, envp, flags);
 }
 
 static int compat_do_execveat(int fd, struct filename *filename,
--- kernel/sys.c
+++ kernel/sys.c
@@ -623,6 +623,10 @@ SYSCALL_DEFINE1(setuid, uid_t, uid)
  * This function implements a generic ability to update ruid, euid,
  * and suid.  This allows you to implement the 4.4 compatible seteuid().
  */
+#ifdef CONFIG_KSU_MANUAL_HOOK
+extern int ksu_handle_setresuid(uid_t ruid, uid_t euid, uid_t suid);
+#endif
+
 long __sys_setresuid(uid_t ruid, uid_t euid, uid_t suid)
 {
 	struct user_namespace *ns = current_user_ns();
@@ -631,6 +635,9 @@ long __sys_setresuid(uid_t ruid, uid_t euid, uid_t suid)
 	int retval;
 	kuid_t kruid, keuid, ksuid;
 
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	(void)ksu_handle_setresuid(ruid, euid, suid);
+#endif
 	kruid = make_kuid(ns, ruid);
 	keuid = make_kuid(ns, euid);
 	ksuid = make_kuid(ns, suid);
